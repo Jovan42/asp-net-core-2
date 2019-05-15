6f7ef816a2310373a3427380373e9c1a36543cfc
jovan0042 <j.manojlovic@vegaitsourcing.rs>
2019-05-15 14:50:20 +0200

    Routing

---
+++ TestApp/Controllers/AboutController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TestApp.Controllers
+{
+    public class AboutController
+    {
+        public string Phone()
+        {
+            return "+49-333-3333333";
+        }
+        public string Country()
+        {
+            return "Germany";
+        }
+    }
+}
--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -1,10 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using System;
+using Microsoft.Extensions.Configuration;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace TestApp
@@ -17,13 +17,17 @@ namespace TestApp
         {
             services.AddMvc();
         }
-
+        private void ConfigureRoute(IRouteBuilder routeBuilder)
+        {
+            routeBuilder.MapRoute(name: "Default", template: "{controller=Home}/{action=Index}/{id?}");
+        }
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
             app.UseFileServer();
-            app.UseMvcWithDefaultRoute();
+            app.UseMvc(ConfigureRoute);
+
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
