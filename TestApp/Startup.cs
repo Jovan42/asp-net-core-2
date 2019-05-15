5ce05211ef34dcb82ebfe774cefb9128b401a1cb
jovan0042 <j.manojlovic@vegaitsourcing.rs>
2019-05-15 14:22:37 +0200

    Setup MVC

--- TestApp/Startup.cs
+++ TestApp/Startup.cs
@@ -15,6 +15,7 @@ namespace TestApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,6 +23,7 @@ namespace TestApp
         {
             app.UseDeveloperExceptionPage();
             app.UseFileServer();
+            app.UseMvcWithDefaultRoute();
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
