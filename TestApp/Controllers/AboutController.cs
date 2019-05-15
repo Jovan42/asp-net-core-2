91164189a621a5c80fafcda527a85e4e667fda86
jovan0042 <j.manojlovic@vegaitsourcing.rs>
2019-05-15 14:57:46 +0200

    Attribute Routes

--- TestApp/Controllers/AboutController.cs
+++ TestApp/Controllers/AboutController.cs
@@ -1,16 +1,20 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace TestApp.Controllers
 {
+    [Route("[controller]")]
     public class AboutController
     {
+        [Route("")]
         public string Phone()
         {
             return "+49-333-3333333";
         }
+        [Route("country")]
         public string Country()
         {
             return "Germany";
